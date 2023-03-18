fa540fb38b32dbe71963bc8384b8a5eac6079157
Kiruidyno <kirui472@gmail.com>
2023-03-18 04:06:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+
+function writeCards(names, eventName) {
+    const messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  
+  const cards = ["Guadalupe", "Ollie", "Aki"];
+  const messages = writeCards(cards, "surprise");
+  
+  for (let i = 0; i < messages.length; i++) {
+    console.log(messages[i]);
+  }
+  
+
+
+  function countDown() {
+    var countdown = 9;
+    while (countdown >= 0) {
+      console.log(countdown);
+      countdown = countdown - 1;
+    }
+    console.log("countdown");
+  }
+  
+countDown();
+  
